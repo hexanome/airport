c983e915a29884b7f6921f3f04cae0f8fd070da7
Jan Keromnes <janx@linux.com>
2011-11-10 12:52:29 +0100

    Work in progress in /web/js/airport.js
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- web/js/airport.js
+++ web/js/airport.js
@@ -1,5 +1,52 @@
 /* airport.js
-   Main package for the airport terminal simulator.
+   Main package for the airport terminal building simulator.
    Copyright (c) 2011-2012 INSA IF Hexanome 4101
  */
  
+var terminal = [];
+
+function Hall(component) {
+  this.baggageHolders = baggageHolders || [];
+}
+
+
+
+function BaggageHolder(baggages, detectors) {
+  this.baggages = baggages || [];
+  this.detectors = detectors || [];
+}
+
+
+
+
+
+function Node(type, links) {
+  // type can be: plane, desk, carrousel, speedwalk, slide.
+  this.type = type || 'plane';
+  this.links = links || [];
+}
+Node.prototype = BaggageHolder.prototype;
+Node.prototype.addLink = function (type, toNode) {
+  var link = new Link(type, this, toNode);
+  this.links.push(link);
+  toNode.links.push(link);
+  return link;
+};
+
+
+
+function Link(type, from, to) {
+  // type can be: train, trail.
+  this.type = type || 'train';
+  this.from = from || new Node();
+  this.to = to || new Node();
+}
+Link.prototype = BaggageHolder.prototype;
+
+
+
+
+function Baggage() {
+  this.
+}
+
