7cc88d96c0a431fae14c68b1f3d143585996335d
raph <raphael.catolino@gmail.com>
2011-11-20 13:43:55 +0100

    index.html is now index1.html

--- local.mk
+++ local.mk
@@ -2,7 +2,7 @@ launch:
 	@echo "launch"
 	@if [ `id -u` -ne "0" -a $(PORT) -lt 1024 ] ;  \
 	then  \
-	  cd web ; sudo node ../server.js ../config.json > ../log.txt ;  \
+	  cd web ; sudo node ../server.js ../config.json ;  \
 	else  \
-	  cd web ; node ../server.js ../config.json > ../log.txt ;  \
+	  cd web ; node ../server.js ../config.json;  \
 	fi
--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 var fs = require('fs'),
     camp = require ('./camp/camp.js');
 
+
 // Start the server
 function start(config) {
 
@@ -19,7 +20,35 @@ function start(config) {
   camp.add('pushconfig', function(newconfig) {
     config = newconfig;
   });
-  
+
+	camp.Plate.macros['l'] = function ( literal, params ) {
+		console.log(params[0]);
+		console.log(params[1]);
+		console.log(params[2]);
+		console.log(camp.Plate.value);
+		/*
+		console.log(path);
+		var list = '';
+    var newliteral = literal;
+    for (var i in val) {
+      newliteral[params[1]] = val[i];
+      newliteral[params[2]] = i;
+      list += literal;
+    }
+		*/
+		return literal;
+	};
+
+  // Add objects from config to index2.html
+	camp.handle('/lol.html', function(query, path) {
+		console.log('templating index');
+		return {
+			wagons: config.airport.wagons,
+			rails: config.airport.rails,
+		  nodes: config.airport.nodes
+		};
+	});
+
   // Display the current config as a JSON file
   camp.handle('/config.json', function(query, path) {
     return {"content":JSON.stringify(config,null,2)};
@@ -32,7 +61,9 @@ function start(config) {
 // Main function
 (function main() {
   var config = process.argv[2] || '../config.json';
-  
+
+	console.log('starting...');
+
   fs.readFile(config, function(err, data) {
     if ( err ) throw err;
     start(JSON.parse(data));
