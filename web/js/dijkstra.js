af0c05a1d0a800818599af0d01e77c8641b6aa1f
raph <raphael.catolino@gmail.com>
2011-11-21 21:37:35 +0100

    +dijsktra.js by alexis Signed-off-by: raph <raphael.catolino@gmail.com>

---
+++ web/js/dijkstra.js
@@ -0,0 +1,169 @@
+// Définition des variables
+
+var infini = 99;
+
+var debut = 0;
+
+// contient les noeuds pour lesquels la recherche s'arrête
+var finit = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+// contient les chaines des noeuds
+var elements = [[0,1,5],
+                [1,0,2],
+                [5,2,0]];
+		
+var nbnoeud = elements.length;
+
+// tableau intermédiaire du déroulement du calcul
+var tableau = []; 
+// remplissage de tableau
+for (i = 0; i < nbnoeud; i++) {
+ tableau[i] = [];  
+}
+
+// tableau contenant la liste ordonnée des noeuds du chemin
+var chemin = [];
+
+// Définition des fonctions
+
+// renvoi la colonne de la plus petite valeur de la ligne donnée
+function minligne (n) {
+var r = 10000;
+var a = -1;
+  
+ for (i = 0; i < nbnoeud; i++) {   
+     if (r > tableau[i][n]) {
+       r = tableau[i][n];       
+       a = i;       
+     }    
+ }   
+ return a;    
+}
+
+// remplit la ligne position du tableau
+function ligne (petit, position) {
+  
+  for (i = 1; i < nbnoeud; i++){  
+    tableau[i][position] = -1;
+  
+    tableau[petit][position] = infini;
+
+    tableau[0][position] = infini;
+  
+    if ((elements[i][petit] !== 0)&&(finit[i] !== 1)) {        
+      tableau[i][position] = +tableau[petit][position-1] + elements[i][petit];
+    }
+  
+    if (tableau[i][position] === -1) {
+      tableau[i][position] = tableau[i][position-1];
+    }
+   
+    if ((tableau[i][position] === infini)&(tableau[i][position-1] !== infini)) {
+      finit[i] = 1;      
+    }
+  }
+}
+
+// fonction principale
+function calcul (elements) {  
+ var minimum = 0;
+
+ // en remplit la deuxième ligne de tableau
+ tableau[0][0] = [0];
+ for (i = 0; i < nbnoeud; i++) {
+    if (i !== 0) {
+     tableau[i][0] = infini;
+    }
+ }  
+ 
+ ligne(0,1);
+ 
+ for (count = 1; count < nbnoeud-1; count++) {
+   ligne(minligne(count),count+1);     
+ }
+  return tableau;  
+}
+
+calcul(elements);
+
+// renvoit la ligne du minimum d'une colonne
+function mincolonne(colonne) {
+
+  var minimum = 1000;
+  var ligne = -1;
+  
+  for (i = 0; i < 10; i++) {
+    if ( minimum > tableau[colonne][i]) {
+      minimum = tableau[colonne][i];
+      ligne = i;
+    }
+  }
+  return ligne;
+}
+
+
+// remplissage de chemin
+
+chemin[nbnoeud] = minligne((mincolonne(nbnoeud-1))-1);
+
+for (countc = nbnoeud-1; countc > 0; countc--){
+  
+  if (chemin[countc-1] !== -1){
+    
+  chemin[countc] = minligne((mincolonne(chemin[countc+1]))-1);
+  }
+  
+  if (chemin[countc] === 0) break;
+}
+
+chemin[nbnoeud + 1] = nbnoeud-1;
+
+while (chemin[0] !== 0){
+  for (deca1 = 0; deca1 < nbnoeud+1; deca1++) {
+    if (chemin[deca1] === 0) {
+      for (deca2 = deca1-1; deca2 < nbnoeud+1; deca2++) {
+        chemin[deca2] = chemin[deca2+1];
+      }
+      chemin[deca2] = -1;
+    }
+  }
+}
+
+/**************************************/
+
+function distance(node1, node2){
+	return Math.sqrt(Math.pow(node1.x-node2.x,2)+Math.pow(node1.y-node2.y,2))
+}
+
+function getElems(elems,start,dest){
+	var temp=elems[0];
+	elems[0]=elems[start];
+	elems[start]=temp;
+	temp=elems[elems.length-1];
+	elems[elems.length-1]=elems[dest];
+	elems[dest]=temp;
+	return elems;
+}
+
+function next(){
+	var nodes=config.airport.nodes;
+	var rails=config.airport.rails;
+	var elems=[[]];
+
+	for (var i in nodes){
+		/*Go through each node to which the wagon can stop (true nodes)*/
+		elems.push([])
+		for (var j in nodes){
+			elems[i].push(0);
+		}
+		/*Find out wich nodes he is linked to and the distance between them */
+		for (var k in rails){
+			if (rails[k].points[0]==i){
+				var last=rails[k].points[rails[k].points.length-1];
+				elems[i][last]=distance(nodes[i],nodes[last]);
+			}
+		}
+	}
+	return elems;
+
+}
