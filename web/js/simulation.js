15b1c4f69f943bc2e199ed058cdca908f2ba464f
Jan Keromnes <janx@linux.com>
2011-11-22 19:16:26 +0100

    Incoming luggage!
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- web/js/simulation.js
+++ web/js/simulation.js
@@ -12,7 +12,16 @@ function addBag() {
     var bags = window.config.airport.bags;
     for(var i in bags ){
       if(bags[i].time === s) {
+        console.log('adding bag',bags[i].id,'to node',bags[i].pos);
         window.nodes[bags[i].pos].bags.push(bags[i]);
+        var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        rect.setAttributeNS(null, "id", 'bag' + bags[i].id);
+        rect.setAttributeNS(null, "fill", "brown");
+        rect.setAttributeNS(null, "x", window.nodes[bags[i].pos].dom.getAttributeNS(null, "x"));
+        rect.setAttributeNS(null, "y", window.nodes[bags[i].pos].dom.getAttributeNS(null, "y"));
+        rect.setAttributeNS(null, "height", 5);
+        rect.setAttributeNS(null, "width", 5);
+        document.getElementsByTagName('svg')[0].appendChild(rect);
       }
     }
   }
