dbbf57feb5749e7ec42bc63045fe23766ef5e3c9
raph <raphael.catolino@gmail.com>
2011-11-22 19:09:02 +0100

    +disjktra more or less (more less than more) working +some nodes going both ways (again) +some other useless things+ lol Signed-off-by: raph <raphael.catolino@gmail.com>

--- web/js/dijkstra.js
+++ web/js/dijkstra.js
@@ -1,132 +1,142 @@
 // Définition des variables
 
-var infini = 99;
+function calculChemin(elements){
 
-var debut = 0;
+	var infini = 99;
 
-// contient les noeuds pour lesquels la recherche s'arrête
-var finit = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+	var debut = 0;
 
-// contient les chaines des noeuds
-var elements = [[0,1,5],
-                [0,0,2],
-                [5,2,0]];
+	// contient les noeuds pour lesquels la recherche s'arrête
+	var finit = [];
+	for (var i=0; i<=nbnoeud; i++){
+		finit.push(0);
+	}
+
+	// contient les chaines des noeuds
+	/*
+	 * var elements = [[0,0,0],
+									[1,0,0],
+									[9,0,0]];
+	*/
+			
+	var nbnoeud = elements.length;
+
+	// tableau intermédiaire du déroulement du calcul
+	var tableau = []; 
+	// remplissage de tableau
+	for (i = 0; i < nbnoeud; i++) {
+	 tableau[i] = [];  
+	}
+
+	// tableau contenant la liste ordonnée des noeuds du chemin
+	var chemin = [];
+
+	// Définition des fonctions
+
+	// renvoi la colonne de la plus petite valeur de la ligne donnée
+	function minligne (n) {
+	var r = 10000;
+	var a = -1;
 		
-var nbnoeud = elements.length;
+	 for (i = 0; i < nbnoeud; i++) {   
+			 if (r > tableau[i][n]) {
+				 r = tableau[i][n];       
+				 a = i;       
+			 }    
+	 }   
+	 return a;    
+	}
 
-// tableau intermédiaire du déroulement du calcul
-var tableau = []; 
-// remplissage de tableau
-for (i = 0; i < nbnoeud; i++) {
- tableau[i] = [];  
-}
+	// remplit la ligne position du tableau
+	function ligne (petit, position) {
+		
+		for (i = 1; i < nbnoeud; i++){  
+			tableau[i][position] = -1;
+		
+			tableau[petit][position] = infini;
 
-// tableau contenant la liste ordonnée des noeuds du chemin
-var chemin = [];
+			tableau[0][position] = infini;
+		
+			if ((elements[i][petit] !== 0)&&(finit[i] !== 1)) {        
+				tableau[i][position] = +tableau[petit][position-1] + elements[i][petit];
+			}
+		
+			if (tableau[i][position] === -1) {
+				tableau[i][position] = tableau[i][position-1];
+			}
+		 
+			if ((tableau[i][position] === infini)&(tableau[i][position-1] !== infini)) {
+				finit[i] = 1;      
+			}
+		}
+	}
 
-// Définition des fonctions
+	// fonction principale
+	function calcul () {  
+	 var minimum = 0;
 
-// renvoi la colonne de la plus petite valeur de la ligne donnée
-function minligne (n) {
-var r = 10000;
-var a = -1;
-  
- for (i = 0; i < nbnoeud; i++) {   
-     if (r > tableau[i][n]) {
-       r = tableau[i][n];       
-       a = i;       
-     }    
- }   
- return a;    
-}
+	 // en remplit la deuxième ligne de tableau
+	 tableau[0][0] = [0];
+	 for (i = 0; i < nbnoeud; i++) {
+			if (i !== 0) {
+			 tableau[i][0] = infini;
+			}
+	 }  
+	 
+	 ligne(0,1);
+	 
+	 for (count = 1; count < nbnoeud-1; count++) {
+		 ligne(minligne(count),count+1);     
+	 }
+		return tableau;  
+	}
 
-// remplit la ligne position du tableau
-function ligne (petit, position) {
-  
-  for (i = 1; i < nbnoeud; i++){  
-    tableau[i][position] = -1;
-  
-    tableau[petit][position] = infini;
+	calcul();
 
-    tableau[0][position] = infini;
-  
-    if ((elements[i][petit] !== 0)&&(finit[i] !== 1)) {        
-      tableau[i][position] = +tableau[petit][position-1] + elements[i][petit];
-    }
-  
-    if (tableau[i][position] === -1) {
-      tableau[i][position] = tableau[i][position-1];
-    }
-   
-    if ((tableau[i][position] === infini)&(tableau[i][position-1] !== infini)) {
-      finit[i] = 1;      
-    }
-  }
-}
+	// renvoit la ligne du minimum d'une colonne
+	function mincolonne(colonne) {
 
-// fonction principale
-function calcul (elements) {  
- var minimum = 0;
-
- // en remplit la deuxième ligne de tableau
- tableau[0][0] = [0];
- for (i = 0; i < nbnoeud; i++) {
-    if (i !== 0) {
-     tableau[i][0] = infini;
-    }
- }  
- 
- ligne(0,1);
- 
- for (count = 1; count < nbnoeud-1; count++) {
-   ligne(minligne(count),count+1);     
- }
-  return tableau;  
-}
-
-calcul(elements);
-
-// renvoit la ligne du minimum d'une colonne
-function mincolonne(colonne) {
-
-  var minimum = 1000;
-  var ligne = -1;
-  
-  for (i = 0; i < 10; i++) {
-    if ( minimum > tableau[colonne][i]) {
-      minimum = tableau[colonne][i];
-      ligne = i;
-    }
-  }
-  return ligne;
-}
+		var minimum = 1000;
+		var ligne = -1;
+		
+		for (i = 0; i < 10; i++) {
+			if ( minimum > tableau[colonne][i]) {
+				minimum = tableau[colonne][i];
+				ligne = i;
+			}
+		}
+		return ligne;
+	}
 
 
 // remplissage de chemin
 
-chemin[nbnoeud] = minligne((mincolonne(nbnoeud-1))-1);
+	chemin[nbnoeud] = minligne((mincolonne(nbnoeud-1))-1);
 
-for (countc = nbnoeud-1; countc > 0; countc--){
-  
-  if (chemin[countc-1] !== -1){
-    
-  chemin[countc] = minligne((mincolonne(chemin[countc+1]))-1);
-  }
-  
-  if (chemin[countc] === 0) break;
-}
+	for (countc = nbnoeud-1; countc > 1; countc--){
+		
+		if (chemin[countc-1] !== -1){
+			
+		chemin[countc] = minligne((mincolonne(chemin[countc+1]))-1);
+		}
+		
+		if (chemin[countc] === 0) break;
+	}
 
-chemin[nbnoeud + 1] = nbnoeud-1;
+	chemin[nbnoeud + 1] = nbnoeud-1;
 
-while (chemin[0] !== 0){
-  for (deca1 = 0; deca1 < nbnoeud+1; deca1++) {
-    if (chemin[deca1] === 0) {
-      for (deca2 = deca1-1; deca2 < nbnoeud+1; deca2++) {
-        chemin[deca2] = chemin[deca2+1];
-      }
-      chemin[deca2] = -1;
-    }
-  }
+	while (chemin[0] !== 0){
+		for (deca1 = 0; deca1 < nbnoeud+1; deca1++) {
+			if (chemin[deca1] === 0) {
+				for (deca2 = deca1-1; deca2 < nbnoeud+1; deca2++) {
+					chemin[deca2] = chemin[deca2+1];
+				}
+				chemin[deca2] = -1;
+			}
+		}
+	}
+	
+	return chemin;
 }
 
 /**************************************/
@@ -136,7 +146,11 @@ function distance(node1, node2){
 }
 
 function reorderDistances(elems,start,dest){
-	var table=elems.slice(0);
+	var table=[];
+	for (var i in elems)
+	{
+		table[i]=elems[i].slice(0);
+	}
 	var temp=table[0];
 
 	//invert first line and start and last line and stop
@@ -169,7 +183,7 @@ function inverse(elems){
 			ne[i][j]=elems[j][i];
 		}
 	}
-	display(ne);
+	return ne;
 }
 function display(table){
 	for (var i in table){
@@ -191,11 +205,19 @@ function findDistances(){
 		for (var k in rails){
 			if (rails[k].points[0]==i){
 				var last=rails[k].points[rails[k].points.length-1];
-				elems[i][last]=distance(nodes[i],nodes[last]);
+				elems[i][last]=0;
+				for (var l=0; l<rails[k].points.length-1; l++){
+					elems[i][last]+=distance(nodes[rails[k].points[l]],nodes[rails[k].points[l+1]]);
+				}
 			}
 		}
 	}
 	elems.pop();
-	return elems;
-
+	return inverse(elems);
+}
+function dijkstra(elems,start,dest)
+{
+	var reverse=reorderDistances(elems,start,dest);
+	var next=calculChemin(reverse);	
+	return next[1];
 }
